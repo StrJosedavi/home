6542d7e4c7f4394e46a35a1b03cbe6c374bb4899
StrJosedavi <josedavi100@gmail.com>
2024-01-28 00:55:53 -0300

    #Alterações na função principal e adicionando Style

---
+++ src/styles/Home.styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+`;
+
+export const Div = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Footer = styled.footer`
+  display: flex;
+  align-items: center;
+`;
+
+export const Section = styled.section`
+  display: flex;
+  align-items: center;
+`;
