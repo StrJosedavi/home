331e0dfc51d0088df795a97cf076e6c53051a7f5
StrJosedavi <josedavi100@gmail.com>
2024-01-28 00:54:24 -0300

    #Criação do componente de navbar Inicial

---
+++ src/styles/navBar.styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+`;
+
+export const List = styled.ul`
+  display: flex;
+  align-items: center;
+`;
+
+export const Line = styled.li`
+  display: flex;
+  align-items: center;
+`;
+
+export const Linker = styled.a`
+  display: flex;
+  align-items: center;
+`;
