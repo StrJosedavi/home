3e91c00a64c8037570cc647aa37084095c898cce
StrJosedavi <josedavi100@gmail.com>
2025-03-26 23:16:25 -0300

    feat: estruturando rotas e adicionando vite ao projeto

---
+++ src/Pages/NotFound.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Main(){
+
+    return (
+        <h1>NOT FOUND</h1>
+    )
+}
+
+export default Main;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from './pages/Home';
+import Home from './Pages/Home';
+import NotFound from './Pages/NotFound';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+
 root.render(
   <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
+    <BrowserRouter basename="/Portfolio">
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
